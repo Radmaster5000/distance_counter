622e247de08b5e35bfdb3d1a2f6284ad5fe08a49
Radmaster5000 <phil.radford5000@gmail.com>
2023-09-15 22:42:08 +0100

    fixed bug that returned a 404 error when trying to return to main site from admin page

--- distance_counter/distance_counter/urls.py
+++ distance_counter/distance_counter/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("distance/", include("distance.urls")),
     path('admin/', admin.site.urls),
+    path("", include("distance.urls"))
 ]
